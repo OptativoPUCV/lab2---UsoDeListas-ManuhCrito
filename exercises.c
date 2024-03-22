fed5e16936f7ae7b7b267d6ac53bad525f231c0d
Your Name <you@example.com>
2024-03-22 20:18:53 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -42,8 +42,12 @@ Al finalizar retorna la lista creada.
 */
 
 List* crea_lista() {
-   List* L = create_list();
-   return L;
+  List* L = create_list();
+  for(int i = 1; i <= 10; i++)
+    {
+      pushBack(L, &i);
+    }
+  return L;
 }
 
 /*
